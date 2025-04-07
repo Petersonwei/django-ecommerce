1db5df1e18c0e857c3a38f332d16cbdd9748e091
petersonwei <weipetersonwei@gmail.com>
2025-04-08 06:04:48 +1000

    fix the cart view issue

--- cart/urls.py
+++ cart/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'cart'
-
+# app_name = 'cart'
 urlpatterns = [
     path('', views.cart_summary, name='cart-summary'),
     path('add/', views.cart_add, name='cart-add'),
