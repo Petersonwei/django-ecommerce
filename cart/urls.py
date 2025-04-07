99f823d6030b79b67f469abb55c4d5896c36552d
petersonwei <weipetersonwei@gmail.com>
2025-04-08 05:54:46 +1000

    fixed the namespace bug & cart summary

--- cart/urls.py
+++ cart/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('', views.cart_summary, name='cart_summary'),
-    path('add/', views.cart_add, name='cart_add'),
-    path('delete/', views.cart_delete, name='cart_delete'),
-    path('update/', views.cart_update, name='cart_update'),
+    path('', views.cart_summary, name='cart-summary'),
+    path('add/', views.cart_add, name='cart-add'),
+    path('delete/', views.cart_delete, name='cart-delete'),
+    path('update/', views.cart_update, name='cart-update'),
 ]
